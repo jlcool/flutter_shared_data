819cc1489bded7f8af92b71a6484ce510d9001b6
姜亮 <>
2019-05-06 11:14:26 +0800

    ios

--- ios/Classes/FlutterSharedDataPlugin.m
+++ ios/Classes/FlutterSharedDataPlugin.m
@@ -1,20 +1,50 @@
 #import "FlutterSharedDataPlugin.h"
 
 @implementation FlutterSharedDataPlugin
+{
+    NSString *sharedText;
+}
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
   FlutterMethodChannel* channel = [FlutterMethodChannel
       methodChannelWithName:@"flutter_shared_data"
             binaryMessenger:[registrar messenger]];
   FlutterSharedDataPlugin* instance = [[FlutterSharedDataPlugin alloc] init];
+    [registrar addApplicationDelegate:instance];
   [registrar addMethodCallDelegate:instance channel:channel];
 }
 
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  if ([@"getSharedPath" isEqualToString:call.method]) {
+    result(sharedText);
+      sharedText=nil;
   } else {
     result(FlutterMethodNotImplemented);
   }
 }
+# pragma mark - AppDelegate
+-(BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url {
 
+    if ([url.absoluteString hasPrefix:@"file://"])
+    {
+       sharedText=url.path;
+    }
+    
+    return YES;
+}
+
+-(BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    if ([url.absoluteString hasPrefix:@"file://"])
+    {
+        sharedText=url.path;
+    }
+    return YES;
+}
+
+-(BOOL)application:(UIApplication *)application openURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
+    if ([url.absoluteString hasPrefix:@"file://"])
+    {
+        sharedText=url.path;
+    }
+    return YES;
+}
 @end
--- test/flutter_shared_data_test.dart
+++ test/flutter_shared_data_test.dart
@@ -16,6 +16,6 @@ void main() {
   });
 
   test('getPlatformVersion', () async {
-    expect(await FlutterSharedData.platformVersion, '42');
+    expect(await FlutterSharedData.getSharedPath, '42');
   });
 }
